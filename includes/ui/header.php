42778a2c5c5e7c9eeb8bb4fe5677616c15ddf80d
Tania <tania@MacBook-Pro-de-nico.local>
2022-10-27 00:48:59 +0200

    organised a template into different files

---
+++ includes/ui/header.php
@@ -0,0 +1,8 @@
+<header class="py-1 bg-light border-bottom mb-4">
+    <div class="container">
+        <div class="text-center my-5">
+            <h1 class="fw-bolder">Hi there !</h1>
+            <p class="lead mb-0">Welcome to my blog !</p>
+        </div>
+    </div>
+</header>
